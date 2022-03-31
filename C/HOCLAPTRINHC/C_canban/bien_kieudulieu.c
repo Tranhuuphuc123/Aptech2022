751ded93f60052d6db981259adf093a5e2e36271
your name <cantho1995ct@gmail.com>
2022-03-31 20:50:58 +0700

    luu thay doi lan 1

--- C/HOCLAPTRINHC/C_canban/bien_kieudulieu.c
+++ C/HOCLAPTRINHC/C_canban/bien_kieudulieu.c
@@ -4,19 +4,19 @@ int main() {
 
 	float a, b, c, d, tong, tich, thuong, hieu;
 	printf("nhap vao so a: ");
-	scanf_s("%.2f", &a);
+	scanf_s("%f", &a);
 	printf("\n");
 
 	printf("nhap vao gia tri b: ");
-	scanf_s("%.2f", &b);
+	scanf_s("%f", &b);
 	printf("\n");
 
 	printf("nhap vao gia tri c: ");
-	scanf_s("%.2f", &c);
+	scanf_s("%f", &c);
 	printf("\n");
 
 	printf("nhap vao gia d: ");
-	scanf_s("%.2f", &d);
+	scanf_s("%f", &d);
 	printf("\n");
 
 	printf("\n");
@@ -26,7 +26,7 @@ int main() {
 	thuong = a / c;
 	hieu = a - b;
 
-	printf("tong cua: %2f + % .2f + %.2f + %.2f = %.2f \n", a, b, c, d, tong);
+	printf("tong cua: %.2f + %.2f + %.2f + %.2f = %.2f \n", a, b, c, d, tong);
 	printf("tich la: %.2f * %.2f = %.2f \n", a, b, tich);
 	printf("thuong la : %.2f/%.2f = %.2f", a, c, thuong);
 	printf("hieu la: %.2f - %.2f = %.2f", a, b, hieu);
